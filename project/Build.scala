2db60f83783c7be5546aeb1fb1d06b943df3082f
Alexandre Mendes <alexandregme@gmail.com>
2013-05-01 17:34:21 -0300

    adequações para funcionar o play2

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,20 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "gestao_hospitalar"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "gestao_hospitalar"
+    val appVersion      = "1.0"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean
-  )
+    val appDependencies = Seq(
+      javaCore,
+      javaJdbc,
+      javaJpa,
+      "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
+      "postgresql" % "postgresql" % "9.1-901.jdbc4"
+    )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      ebeanEnabled := false   
+    )
 
 }
+            
